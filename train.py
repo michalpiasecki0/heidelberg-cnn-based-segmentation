aadc2e1f75cb83df0e4895f1c786a6c405ec8274
Mateusz Borowski <m.borowski128@gmail.com>
2025-05-21 15:49:17 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,61 +1,34 @@
+import hydra
 import pytorch_lightning as pl
-import torch
 from clearml import Task
+from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
-from torchvision import transforms
 
-from src.constants import FLUO_PATH, LossWeights
-from src.datasets import SegmentationDataset
-from src.unet_lightning import UNetSegmentationModule
+
+def train(cfg: DictConfig) -> None:
+    model_lt = hydra.utils.instantiate(cfg.model)
+
+    dataset_train = hydra.utils.instantiate(cfg.dataset.train)
+    dataset_val = hydra.utils.instantiate(cfg.dataset.val)
+
+    dataloader = DataLoader(dataset=dataset_train, batch_size=cfg.batch_size)
+    val_dataloader = DataLoader(dataset=dataset_val, batch_size=cfg.batch_size)
+
+    trainer = pl.Trainer(max_epochs=cfg.max_epochs)
+    trainer.fit(model_lt, train_dataloaders=dataloader, val_dataloaders=val_dataloader)
+
+
+@hydra.main(version_base="1.3", config_path="configs", config_name="train.yaml")
+def main(cfg: DictConfig) -> None:
+    # todo: uncomment this when using clearml
+    # task = Task.init(project_name=cfg.tracker.project_name, task_name=cfg.tracker.task_name)
+
+    OmegaConf.resolve(cfg)
+    train(cfg)
+
+    # todo: uncomment this when using clearml
+    # task.mark_completed()
+
 
 if __name__ == "__main__":
-    # --- ClearML Task initialization ---
-    task = Task.init(project_name="UNet Segmentation", task_name="Test")
-
-    # Example usage
-    input_dim = 1
-    output_dim = 1
-    learning_rate = 1e-3
-    unet_kwargs = {
-        "input_dim": input_dim,
-        "output_dim": output_dim,
-        "hidden_dims": [64, 128, 256, 512, 1024],
-        "kernel_size": 3,
-        "padding_mode": "same",
-        "skip_mode": "concat",
-        "upsampling_mode": "transpose",
-        "dropout": 0,
-    }
-
-    loss_weights = LossWeights(
-        cross_entropy=1.0,  # weight for Cross Entropy loss
-        dice=1.0,  # weight for Dice loss
-    )
-
-    model_lt = UNetSegmentationModule(
-        unet_kwargs, learning_rate, loss_weights=loss_weights
-    )
-
-    dataset_fluo = SegmentationDataset(
-        root_path=FLUO_PATH,
-        img_folder="01",
-        target_folder="01_ST/SEG",
-        transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Resize((512, 512))],
-        ),
-        target_transform=transforms.Resize((512, 512)),
-    )
-    dataset_fluo_val = SegmentationDataset(
-        root_path=FLUO_PATH,
-        img_folder="02",
-        target_folder="02_ST/SEG",
-        transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Resize((512, 512))],
-        ),
-        target_transform=transforms.Resize((512, 512)),
-    )
-
-    dataloader = DataLoader(dataset=dataset_fluo, batch_size=4)
-    val_dataloader = DataLoader(dataset=dataset_fluo_val, batch_size=4)
-    trainer = pl.Trainer(max_epochs=3)
-    trainer.fit(model_lt, train_dataloaders=dataloader, val_dataloaders=val_dataloader)
+    main()
