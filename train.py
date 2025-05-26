9eef3089963e8e0f701485bd24281f95058bf887
Mateusz Borowski <m.borowski128@gmail.com>
2025-05-26 19:19:13 +0200

    Format train script

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from lightning.pytorch.loggers import WandbLogger
 from datetime import datetime
 
+
 def train(cfg: DictConfig) -> None:
     model_lt = hydra.utils.instantiate(cfg.model)
 
@@ -14,9 +15,9 @@ def train(cfg: DictConfig) -> None:
 
     dataloader = DataLoader(dataset=dataset_train, batch_size=cfg.batch_size)
     val_dataloader = DataLoader(dataset=dataset_val, batch_size=cfg.batch_size)
-    
+
     # get dataset path for project name
-    dataset_path = (hydra.utils.instantiate(cfg.dataset.train.root_path).name)
+    dataset_path = hydra.utils.instantiate(cfg.dataset.train.root_path).name
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     wand_logger = WandbLogger(
         project=dataset_path,
@@ -24,17 +25,15 @@ def train(cfg: DictConfig) -> None:
         log_model=True,
         config=OmegaConf.to_container(cfg, resolve=True),
     )
-    trainer = pl.Trainer(max_epochs=cfg.max_epochs, 
-                         logger=wand_logger)
+    trainer = pl.Trainer(max_epochs=cfg.max_epochs, logger=wand_logger)
     trainer.fit(model_lt, train_dataloaders=dataloader, val_dataloaders=val_dataloader)
-    
+
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="train.yaml")
 def main(cfg: DictConfig) -> None:
 
     OmegaConf.resolve(cfg)
     train(cfg)
-
 
 
 if __name__ == "__main__":
