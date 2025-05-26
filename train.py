ce4e8dead35ce563078b2e625f41137f969ca4d3
Misha Piasecki <michal.piasecki0@gmail.com>
2025-05-26 15:37:27 +0200

    modify names in train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from clearml import Task
 from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
 from lightning.pytorch.loggers import WandbLogger
-
+from datetime import datetime
 
 def train(cfg: DictConfig) -> None:
     model_lt = hydra.utils.instantiate(cfg.model)
@@ -14,28 +14,27 @@ def train(cfg: DictConfig) -> None:
 
     dataloader = DataLoader(dataset=dataset_train, batch_size=cfg.batch_size)
     val_dataloader = DataLoader(dataset=dataset_val, batch_size=cfg.batch_size)
-
+    
+    # get dataset path for project name
+    dataset_path = (hydra.utils.instantiate(cfg.dataset.train.root_path).name)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     wand_logger = WandbLogger(
-        project=cfg.tracker.project_name,
-        name=cfg.tracker.task_name,
+        project=dataset_path,
+        name=current_time,
         log_model=True,
         config=OmegaConf.to_container(cfg, resolve=True),
     )
     trainer = pl.Trainer(max_epochs=cfg.max_epochs, 
                          logger=wand_logger)
     trainer.fit(model_lt, train_dataloaders=dataloader, val_dataloaders=val_dataloader)
-
+    
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="train.yaml")
 def main(cfg: DictConfig) -> None:
-    # todo: uncomment this when using clearml
-    #task = Task.init(project_name=cfg.tracker.project_name, task_name=cfg.tracker.task_name)
 
     OmegaConf.resolve(cfg)
     train(cfg)
 
-    # todo: uncomment this when using clearml
-    #task.mark_completed()
 
 
 if __name__ == "__main__":
